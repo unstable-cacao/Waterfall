42b4eaa5b1ec86bd4ec16b68fa1e237f6a56e190
Marina Sherstuk <marinashers@gmail.com>
2017-01-11 21:26:58 +0200

    Populated payload object

--- src/Waterfall/Core/Objects/Payload.php
+++ src/Waterfall/Core/Objects/Payload.php
@@ -2,7 +2,28 @@
 namespace Waterfall\Core\Objects;
 
 
-class Payload
-{
+use Objection\LiteObject;
+use Objection\LiteSetup;
 
+
+/**
+ * @property int		$ID
+ * @property \DateTime	$Created
+ * @property \DateTime	$Modified
+ * @property string		$Data
+ */
+class Payload extends LiteObject
+{
+	/**
+	 * @return array
+	 */
+	protected function _setup()
+	{
+		return [
+			'ID' 		=> LiteSetup::createInt(null),
+			'Created' 	=> LiteSetup::createDateTime(),
+			'Modified' 	=> LiteSetup::createDateTime(),
+			'Data' 		=> LiteSetup::createString(null)
+		];
+	}
 }
